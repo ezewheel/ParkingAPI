d1de11e2a520bef7de58b7dab88f32140c8a204b
Ezequiel Rueda <ezewheel@gmail.com>
2024-10-30 20:11:14 -0300

    Agregar archivos de proyecto.

---
+++ Data/Migrations/20241030174236_third-migration.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class thirdmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Fee",
+                table: "Parkings",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExitTime",
+                table: "Parkings",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Fee",
+                table: "Parkings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExitTime",
+                table: "Parkings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
---
+++ Services/ParkingService/ParkingService.cs
@@ -0,0 +1,85 @@
+﻿using Common.DTOs.ParkingDTOs;
+using Data.Entities;
+using Data.Repositories.ParkingRepository;
+using Services.FeesService;
+
+namespace Services.ParkingService
+{
+    public class ParkingService : IParkingService
+    {
+        private readonly IParkingRepository _parkingRepository;
+        private readonly IFeesService _feesService;
+        private readonly ISlotsRepository _slotsRepository;
+
+        public ParkingService(IParkingRepository parkingRepository, IFeesService feesService , ISlotsRepository slotsRepository)
+        {
+            _parkingRepository = parkingRepository;
+            _feesService = feesService;
+            _slotsRepository = slotsRepository;
+        }
+
+        public List<ParkingForViewDto> Get()
+        {
+            List<Parking> parkings = _parkingRepository.Get();
+            return parkings.Select(parking => new ParkingForViewDto
+            {
+                Plate = parking.Plate,
+                SlotId = parking.SlotId,
+                EntryTime = parking.EntryTime,
+                ExitTime = parking.ExitTime,
+                Fee = parking.Fee
+            }).ToList();
+        }
+
+        public void Add(ParkingForAddDto parkingForAdd)
+        {
+            _parkingRepository.Add(new Parking
+            {
+                SlotId = parkingForAdd.SlotId,
+                Plate = parkingForAdd.Plate,
+                EntryTime = DateTime.UtcNow,
+                ExitTime = null,
+                Fee = null
+            });
+
+            Slot? slot = _slotsRepository.Get().FirstOrDefault(s => s.Id == parkingForAdd.SlotId);
+            _slotsRepository.Modify(new Slot
+            {
+                Id = slot.Id,
+                Description = slot.Description,
+                IsAvailable = false
+            });
+        }
+
+        public ParkingForViewDto? Close(string plate)
+        {
+            Parking? parking = _parkingRepository.Get(plate);
+            if (parking is null)
+            {
+                return null;
+            }
+
+            DateTime exitTime = DateTime.UtcNow;
+            parking.ExitTime = exitTime;
+            parking.Fee = _feesService.GetFee(parking.EntryTime, exitTime);
+
+            _parkingRepository.Modify(parking);
+            Slot? slot = _slotsRepository.Get().FirstOrDefault(s => s.Id == parking.SlotId);
+            _slotsRepository.Modify(new Slot
+            {
+                Id = slot.Id,
+                Description = slot.Description,
+                IsAvailable = true
+            });
+
+            return new ParkingForViewDto
+            {
+                Plate = parking.Plate,
+                SlotId = parking.SlotId,
+                EntryTime = parking.EntryTime,
+                ExitTime = parking.ExitTime,
+                Fee = parking.Fee
+            };
+        }
+    }
+}
